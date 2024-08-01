0d6227ad4a8695af5801455c9947e30f5130e531
Liam O'Shaughnessy <110779719+liamos7@users.noreply.github.com>
2024-08-01 11:58:51 -0400

    Update neighbor_density.py

--- neighbor_density.py
+++ neighbor_density.py
@@ -1,3 +1,5 @@
+# produces histograms of the number of objects about an object ("neighbor density")
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,6 +8,7 @@ data=np.loadtxt("cosmosk/cat_files/hscg.cat", skiprows=1)
 bit_a1689_scale = .140843
 bit_cosmos_scale = .140732
 
+# 150 is a1689, 197 is cosmosk
 alpha_max = 150.300 #197.9150 
 alpha_min = 150.155 #197.8255
 delta_max = 2.345 #-1.210
@@ -20,28 +23,18 @@ for obj in data:
 print('Window cut completed')
 flux = []
 coords = []
-ang = []
-bit_back = []
-err = []
 
 print('Starting to form parameter arrays')
 for obj in objs:
     flux.append(obj[3])
     coords.append([obj[11],obj[12]])
-    #ang.append(bit_cosmos_scale*obj[13])
-    #err.append(obj[4])
-    #bit_back.append(obj[5])
 
 print('Parameter arrays completed')
 flux = np.array(flux)
 coords = np.array(coords)
-ang = np.array(ang)
-bit_back = np.array(bit_back)
-err = np.array(err)
 
-
-cut = np.mean(flux)
-radius = 0.005
+cut = np.mean(flux) # for "bright" neighbor check
+radius = 0.005 # how large of circle want to consider
 densities = []
 
 print('Starting density checking')
@@ -61,7 +54,7 @@ plt.xlabel('# of bright objects in radius ' + str(radius) + ' degrees about obj'
 plt.ylabel('Frequency')
 #plt.title('Neighbor Density (total)')
 plt.title('Neighbor Density (Objects brighter than mean flux)')
-plt.savefig('cosmosk/density_plots/neighbor/hscg_density_cut.png')
+#plt.savefig('cosmosk/density_plots/neighbor/hscg_density_cut.png')
 plt.show()
 
 
